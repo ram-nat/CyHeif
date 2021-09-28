9a05cd72d7fc3b273c84b3ed55b03004dab8ed8c
Ramkumar Natarajan <software@nramkumar.org>
2021-09-27 21:07:20 -0700

    Update package information, source distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import pathlib
 from setuptools.extension import Extension
 import sys
 import os
@@ -9,6 +10,11 @@ argparser.add_argument('--use-cython', action='store_true', default=False)
 argparser.add_argument('--libheif_path', default='C:/vcpkg/installed/x64-windows')
 args, unknown = argparser.parse_known_args()
 sys.argv = [sys.argv[0]] + unknown
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 lib_dirs = []
 inc_dirs = []
@@ -39,6 +45,14 @@ if args.use_cython:
 
 setup(
     name='cyheif',
+    version='0.0.1',
+    description='Python wrapper for libheif',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ram-nat/CyHeif/',
+    author='Ramkumar Natarajan',
+    keywords='libheif, heif, heic, high-efficiency image format',
+    python_requires='>=3.6',
     ext_modules=extensions,
     packages=['cyheifloader']
 )
