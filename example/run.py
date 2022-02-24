65f5943d27c678de67df9d6116a63513d7bfb6c0
Ramkumar Natarajan <ram@nramkumar.org>
2022-02-23 19:32:54 -0800

    Update example to take file name input from command line

--- example/run.py
+++ example/run.py
@@ -8,17 +8,21 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 # Print libheif version
-print(cyheif.get_heif_version())
+print(f'LibHeif Version: {cyheif.get_heif_version()}')
+
+file_name = sys.argv[1] if len(sys.argv) > 1 else 'sample.heic'
+file_name = str.encode(file_name, 'UTF-8')
+print(f'Processing {file_name}')
 
 # Print EXIF data from HEIF file
 print('Getting EXIF Data from EXIF')
-exif = cyheif.get_exif_data(b'sample.heic')
+exif = cyheif.get_exif_data(file_name)
 exif_readable = {TAGS.get(k):v for (k,v) in exif.items()}
 print(exif_readable)
 
 # Get pillow image from HEIF file
 print('Getting Pillow Image from HEIC')
-pil_img = cyheif.get_pil_image(b'sample.heic')
+pil_img = cyheif.get_pil_image(file_name)
 pil_img_exif = pil_img.getexif()
 
 # Resize pillow image
@@ -30,11 +34,12 @@ pil_img.show()
 # Save pillow image as jpeg
 print('Saving Pillow Image as JPEG')
 pil_img.save('sample.jpg', 'JPEG', exif=pil_img.getexif().tobytes())
+pil_img.save('sample.png', 'PNG', exif=pil_img.getexif().tobytes())
 
 # Update EXIF data in input HEIF file and save it to output HEIF file
 print('Updating EXIF data in HEIC')
 pil_img_exif[0x010F] = 'A test image for CyHeif'
-cyheif.write_exif_data(b'sample.heic', b'sample_exif.heic', pil_img_exif)
+cyheif.write_exif_data(file_name, b'sample_exif.heic', pil_img_exif)
 
 # Write pillow image to output HEIF file
 print('Writing Pillow Image to HEIC')
